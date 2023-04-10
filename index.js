f8828d94d18a6bc884def186ed1ee0b03262f3d2
jaylinrose <jaylin.manuel02@gmail.com>
2023-04-09 21:38:44 -0700

    added direction change function modification to move.js and index.js, character moves in all directions with working corresponding animations

--- index.js
+++ index.js
@@ -1,56 +1,27 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-
 const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250;
-move(character).to(100, 250)
 
-
-                                  
-setInterval(function(){ 
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
     if(direction === 'west'){
-        x = x - 1
+        character.src = 'assets/green-character/west.gif'
     }
     if(direction === 'north'){
-        y = y + 1
+        character.src = 'assets/green-character/north.gif'
     }
     if(direction === 'east'){
-        x = x + 1
+        character.src = 'assets/green-character/east.gif'
     }
     if(direction === 'south'){
-        y = y - 1
+        character.src = 'assets/green-character/south.gif'
     }
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
-}, 1)
+}
 
-document.addEventListener('keydown', function(e){
-    if(e.repeat) return;
-
-    if(e.key === 'ArrowLeft'){
-        direction = 'west'
-    }
-    if(e.key === 'ArrowUp'){
-        direction = 'north'
-    }
-    if(e.key === 'ArrowRight'){
-        direction = 'east'
-    }
-    if(e.key === 'ArrowDown'){
-        direction = 'south'
-    }
-})
-
-document.addEventListener('keyup', function(e){
-    direction = null
-})
-
-
-
-
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
